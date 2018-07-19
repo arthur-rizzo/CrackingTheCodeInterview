cadb1657116578c32488e1e5d05a2eb1c8f204ac
Arthur Rizzo <arthur.c.rizzo@gmail.com>
2018-07-19 18:12:37 -0300

    Arrays and String complete

--- CrackingTheCodeInterview/ArraysAndStrings.cs
+++ CrackingTheCodeInterview/ArraysAndStrings.cs
@@ -63,6 +63,228 @@ namespace CrackingTheCodeInterview._01._Array_And_Strings
 			return occurrencesCounter;
 		}
 
+		public static string URLify(string url, int realLength)
+		{
+			if (realLength < 0)
+				throw new ArgumentOutOfRangeException($"{nameof(realLength)} must be greater or equal to zero");
+			if (string.IsNullOrWhiteSpace(url))
+				return string.Empty;
 
+			StringBuilder builder = new StringBuilder();
+			for (int i = 0; i < realLength; i++)
+			{
+				char c = url[i];
+				if (c == ' ')
+					builder.Append("%20");
+				else
+					builder.Append(c);
+			}
+
+			return builder.ToString();
+		}
+
+		public static bool IsPalindromePermutation(string s)
+		{
+			if (string.IsNullOrEmpty(s))
+				return true;
+
+			s = s.ToLowerInvariant();
+			Dictionary<char, int> occurences = new Dictionary<char, int>();
+			foreach (char c in s)
+			{
+				if (occurences.ContainsKey(c))
+					occurences[c]++;
+				else
+					occurences[c] = 1;
+			}
+
+			//Se uma string tem tamanho impar, teremos X
+			int requiredEvenCharacters = s.Length % 2 == 0 ? occurences.Keys.Count : occurences.Keys.Count - 1;
+			var evenCharacters = occurences.Values.Where(c => c % 2 == 0).Count();
+
+			return requiredEvenCharacters == evenCharacters;
+		}
+
+		public static bool OneEditAway(string s1, string s2)
+		{
+			if (s1 == null)
+				s1 = "";
+
+			if (s2 == null)
+				s2 = "";
+
+			//Could be one replacement away
+			if (s2.Length == s1.Length)
+			{
+				int differences = 0;
+				for (int i = 0; i < s2.Length; i++)
+				{
+					if (s1[i] != s2[i])
+						differences++;
+				}
+
+				return differences <= 1;
+			}
+			else if (Math.Abs(s1.Length - s2.Length) > 1) //a diferenca de tamanho já é maior que 1;
+				return false;
+			else //1 char difference (adding to the smaller means the same than to remove from the bigger)
+			{
+				string bigger, smaller;
+				extractBiggerAndSmallerStrings(s1, s2, out bigger, out smaller);
+
+				int indexSmaller = 0;
+				int differencesCount = 0;
+				for (int indexBigger = 0; indexBigger < bigger.Length; indexBigger++)
+				{
+					if (indexSmaller < smaller.Length && bigger[indexBigger] == smaller[indexSmaller])
+					{
+						indexSmaller++;
+						continue;
+					}
+					else
+					{
+						differencesCount++;
+						if (differencesCount > 1)
+							break;
+
+						continue;
+					}
+				}
+
+				return differencesCount <= 1;
+			}
+
+		}
+		private static void extractBiggerAndSmallerStrings(string s1, string s2, out string bigger, out string smaller)
+		{
+			smaller = "";
+			if (s1.Length > s2.Length)
+			{
+				bigger = s1;
+				smaller = s2;
+			}
+			else
+			{
+				bigger = s2;
+				smaller = s1;
+			}
+		}
+
+		public static string StringCompression(string s)
+		{
+			if (string.IsNullOrEmpty(s))
+				return string.Empty;
+
+			int counter = 0;
+			char currentChar = ' ';
+
+			StringBuilder builder = new StringBuilder();
+
+			foreach (char c in s)
+			{
+				if (currentChar == ' ')
+					currentChar = c;
+
+				if (c == currentChar)
+					counter++;
+				else
+				{
+					builder.Append(currentChar + counter.ToString());
+
+					currentChar = c;
+					counter = 1;
+				}
+			}
+
+			builder.Append(currentChar + counter.ToString());
+
+			var result = builder.ToString();
+			if (result.Length >= s.Length)
+				result = s;
+
+			return result;
+		}
+
+		private static void matrixRotation(int[][] matrix, int diagonalIndex)
+		{
+			int lineSize = matrix.Length - (diagonalIndex * 2);
+			if (lineSize <= 1 || diagonalIndex < 0)
+				return;
+
+			//faz o trocandinho do subquadrado iniciado na coordenada (x,x) onde x é diagonalIndex
+			for (int lineIndex = 0; lineIndex < (lineSize - 1); lineIndex++)
+			{
+				//Performs 4 swaps in the following order: top > left > bottom > right
+				//defines the 4 elements positions to be rotated in the current iteration
+				var topElementX = 0 + diagonalIndex;
+				var topElementY = lineIndex + diagonalIndex;
+
+				var leftElementX = ((lineSize - lineIndex) - 1) + diagonalIndex;
+				var leftElementY = 0 + diagonalIndex;
+
+				var bottomElementX = (lineSize - 1) + diagonalIndex;
+				var bottomElementY = ((lineSize - lineIndex) - 1) + diagonalIndex;
+
+				var rightElementX = lineIndex + diagonalIndex;
+				var rightElementY = (lineSize - 1) + diagonalIndex;
+
+				//defines the elements in variables and perform swaps
+				var topElement = matrix[topElementX][topElementY];
+				var leftElement = matrix[leftElementX][leftElementY];
+				var bottomElement = matrix[bottomElementX][bottomElementY];
+				var rightElement = matrix[rightElementX][rightElementY];
+
+				matrix[leftElementX][leftElementY] = topElement;
+				matrix[bottomElementX][bottomElementY] = leftElement;
+				matrix[rightElementX][rightElementY] = bottomElement;
+				matrix[topElementX][topElementY] = rightElement;
+			}
+
+			matrixRotation(matrix, diagonalIndex - 1);
+		}
+		public static void MatrixRotation(int[][] matrix)
+		{
+			int diagonalSize = matrix.Length;
+			int diagonalEndIndex = (diagonalSize / 2) - 1;
+
+			matrixRotation(matrix, diagonalEndIndex);
+		}
+
+		public static void ZeroMatrix(int[][] matrix)
+		{
+			HashSet<int> rows = new HashSet<int>();
+			HashSet<int> columns = new HashSet<int>();
+
+			for (int i = 0; i < matrix.Length; i++)
+			{
+				for (int j = 0; j < matrix[i].Length; j++)
+				{
+					if (matrix[i][j] == 0)
+					{
+						rows.Add(i);
+						columns.Add(j);
+					}
+				}
+			}
+
+			foreach(var x in rows)
+			{
+				for (int i = 0; i < matrix[0].Length; i++)
+					matrix[x][i] = 0;
+			}
+
+			foreach (var x in columns)
+			{
+				for (int i = 0; i < matrix.Length; i++)
+					matrix[i][x] = 0;
+			}
+
+		}
+
+		public static bool IsRotation(string s1, string s2)
+		{
+			string comparerString = s2 + s2;
+			return s1.Length == s2.Length && comparerString.Contains(s1);
+		}
 	}
 }
--- Test/ArryasAndStringTest.cs
+++ Test/ArryasAndStringTest.cs
@@ -1,5 +1,8 @@
+using System;
 using CrackingTheCodeInterview._01._Array_And_Strings;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Diagnostics;
 
 namespace Test
 {
@@ -29,6 +32,136 @@ namespace Test
 		public void IsPermutation(string a, string b, bool expected)
 		{
 			Assert.AreEqual(expected, ArraysAndStrings.CheckPermutation(a, b));
+		}
+
+		[TestMethod]
+		[DataRow(null, 0, "")]
+		[DataRow("", 0, "")]
+		[DataRow(" ", 0, "")]
+		[DataRow("   ", 0, "")]
+		[DataRow("a a  ", 3, "a%20a")]
+		public void URLfy(string s, int length, string result)
+		{
+			Assert.AreEqual(result, ArraysAndStrings.URLify(s, length));
+		}
+
+		[TestMethod]
+		[DataRow("", true)]
+		[DataRow("a", true)]
+		[DataRow("   ", true)]
+		[DataRow("aa ", true)]
+		[DataRow("bb  ccc", true)]
+		[DataRow("aoiuuioa22xyy", true)]
+		[DataRow("aoiuuioa222xyy", false)]
+		public void IsPalindromePermutation(string s, bool result)
+		{
+			Assert.AreEqual(result, ArraysAndStrings.IsPalindromePermutation(s));
+		}
+
+		[TestMethod]
+		[DataRow("", " ", true)]
+		[DataRow(" ", "", true)]
+		[DataRow("a", " ", true)]
+		[DataRow("", "a", true)]
+		[DataRow(" a", " ", true)]
+		[DataRow("a ", " ", true)]
+		[DataRow("caralho", "caralh", true)]
+		[DataRow("caralho", "aralho", true)]
+		[DataRow("caralo", "caralho", true)]
+		[DataRow("caralbo", "caralho", true)]
+		[DataRow("   b ", "   ", false)]
+		[DataRow("xyzh", "xyzha", true)]
+		[DataRow(" ", "   ", false)]
+		[DataRow("caralbo", "caralho ", false)]
+		public void OneEditAway(string s1, string s2, bool result)
+		{
+			Assert.AreEqual(result, ArraysAndStrings.OneEditAway(s1,s2));
+		}
+
+		[TestMethod]
+		[DataRow("", "")]
+		[DataRow(null, "")]
+		[DataRow("a", "a")]
+		[DataRow("ab", "ab")]
+		[DataRow("aa", "aa")]
+		[DataRow("aab", "aab")]
+		[DataRow("aabbb", "a2b3")]
+		[DataRow("aaabbbaaaaacde", "a3b3a5c1d1e1")]
+		public void StringCompression(string input, string result)
+		{
+			Assert.AreEqual(result, ArraysAndStrings.StringCompression(input));
+		}
+
+		[TestMethod]
+		[DataRow("1","1")]
+		[DataRow("1,2|3,4", "2,4|1,3")]
+		[DataRow("10,11,12,13,14|15,16,17,18,19|20,21,22,23,24|25,26,27,28,29|30,31,32,33,34", "14,19,24,29,34|13,18,23,28,33|12,17,22,27,32|11,16,21,26,31|10,15,20,25,30")]
+		public void MatrixRotation(string inputString, string resultString)
+		{
+			int[][] input = convertToArray(inputString);
+			Trace.WriteLine("Input:");
+			printMatrix(input);
+
+			ArraysAndStrings.MatrixRotation(input);
+
+			Trace.WriteLine("Result");
+			printMatrix(input);
+
+			int[][] result = convertToArray(resultString);
+			for (int i = 0; i < input.Length; i++)
+				for(int j = 0; j < input[i].Length; j++)
+					Assert.AreEqual(input[i][j], result[i][j]);
+		}
+
+		[TestMethod]
+		[DataRow("1", "1")]
+		[DataRow("1,2|3,0", "1,0|0,0")]
+		[DataRow("1,2,1|3,0,2|1,1,1", "1,0,1|0,0,0|1,0,1")]
+		public void ZeroMatrix(string inputString, string resultString)
+		{
+			int[][] input = convertToArray(inputString);
+			Trace.WriteLine("Input:");
+			printMatrix(input);
+
+			ArraysAndStrings.ZeroMatrix(input);
+
+			Trace.WriteLine("Result");
+			printMatrix(input);
+
+			int[][] result = convertToArray(resultString);
+			for (int i = 0; i < input.Length; i++)
+				for (int j = 0; j < input[i].Length; j++)
+					Assert.AreEqual(input[i][j], result[i][j]);
+		}
+
+		private void printMatrix(int[][] input)
+		{
+			foreach (var i in input)
+			{
+				foreach(var j in i)
+				{
+					Trace.Write(j);
+					Trace.Write(" ");
+				}
+				Trace.WriteLine("");
+			}
+		}
+		private int[][] convertToArray(string v)
+		{
+			return v.Split('|').Select(x => x.Split(',').Select(int.Parse).ToArray()).ToArray();
+		}
+
+		[TestMethod]
+		[DataRow("","",true)]
+		[DataRow("a", "aa", false)]
+		[DataRow("ab", "ab", true)]
+		[DataRow("abcdef", "bcdefa", true)]
+		[DataRow("abcdef", "bcdef", false)]
+		[DataRow("abcbcdef", "bcdefabc", true)]
+		public void IsRotation(string s1, string s2, bool expected)
+		{
+			bool result = ArraysAndStrings.IsRotation(s1, s2);
+			Assert.AreEqual(expected, result);
 		}
 	}
 }
